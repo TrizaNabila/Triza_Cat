bb78facb94dd36b651d42316b749a15414cb8081
triza <triza24si@mahasiswa.pcr.ac.id>
2025-10-14 11:41:02 +0700

    CRUD p6

--- routes/web.php
+++ routes/web.php
@@ -3,7 +3,10 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 use App\Http\Controllers\PegawaiController;
+use App\Http\Controllers\QuestionController;
+use App\Http\Controllers\DashboardController;
 use App\Http\Controllers\MahasiswaController;
+use App\Http\Controllers\PelangganController;
 
 
 Route::get('/', function () {
@@ -30,3 +33,12 @@ Route::get('/about', function () {
 });
 Route:: get ('/blade',[PegawaiController::class,'index']);
 
+Route::post('question/store', [QuestionController::class, 'store'])
+		->name('question.store');
+
+Route:: get ('/home',[HomeController::class,'index']);
+
+Route::resource('pelanggan', PelangganController::class);
+
+Route::get('dashboard', [DashboardController::class, 'index'])
+        ->name('dashboard');
